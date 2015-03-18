7e25bd97220adeef5ffc324db7b14cc6513fd867
Richard Kimber <richard@dogma.co.uk>
2015-03-18 10:47:12 +0000

    Poodle fix

--- lib/active_utils/common/connection.rb
+++ lib/active_utils/common/connection.rb
@@ -102,6 +102,7 @@ module ActiveMerchant
       return unless endpoint.scheme == "https"
 
       http.use_ssl = true
+      http.ssl_version = :TLSv1_2
 
       if verify_peer
         http.verify_mode = OpenSSL::SSL::VERIFY_PEER
